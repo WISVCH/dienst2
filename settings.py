b33a24b00ab96444e8a60e04d53a0a5eaf488077
Mark Janssen <mark@praseodym.net>
2011-10-18 23:31:34 +0200

    LOGIN_REDIRECT_URL

--- settings.py
+++ settings.py
@@ -76,6 +76,8 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'dienst2.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
