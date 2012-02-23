f17aaa292fec2137a4037bf226b178c8f16f682d
Mark Janssen <mark@praseodym.net>
2012-02-23 16:55:00 +0100

    Add warning to ldap.OPT_X_TLS_REQUIRE_CERT

--- urls.py
+++ urls.py
@@ -10,7 +10,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url( r'^$', index, name='index'),
+    url(r'^$', index, name='index'),
     
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
