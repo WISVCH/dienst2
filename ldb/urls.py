640e9cb91ea4241175c7820781e9dcd6636a6bff
Mark Janssen <mark@praseodym.net>
2015-05-17 17:13:00 +0200

    Initial restore of old templates

--- ldb/urls.py
+++ ldb/urls.py
@@ -5,6 +5,8 @@ from tastypie.api import Api
 from ldb import views_api
 from ldb.api import *
 from ldb.export import ExportResource
+from ldb.views import PersonDetailView, PersonDeleteView, person_edit, OrganizationDetailView, OrganizationDeleteView, \
+    organization_edit, ajax_people_search, index_old
 
 api = Api(api_name='v2')
 
@@ -23,11 +25,29 @@ router = routers.DefaultRouter()
 router.register(r'people', views_api.PersonViewSet)
 router.register(r'organizations', views_api.OrganizationsViewSet)
 
-urlpatterns = patterns('',
-                       (r'^api/', include(api.urls)),
+urlpatterns = patterns(
+    '',
+    (r'^api/', include(api.urls)),
 
-                       url(r'^$', 'ldb.views.index', name="ldb_index"),
+    url(r'^$', 'ldb.views.index', name="ldb_index"),
 
-                       url(r'^api/v3/', include(router.urls)),
-                       # url(r'^api/v3/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/v3/', include(router.urls)),
+    # url(r'^api/v3/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+    url(r'^old/$', index_old, name='ldb_index_old'),
+
+    url(r'^people/search/$', ajax_people_search, name='ldb_people_search' ),
+
+    url(r'^people/(?P<pk>\d+)/$', PersonDetailView.as_view(), name='ldb_people_detail'),
+    url(r'^people/(?P<pk>\d+)/delete/$', PersonDeleteView.as_view(), name='ldb_people_delete'),
+    url(r'^people/(?P<pk>\d+)/edit/$', person_edit),
+    url(r'^people/create/$', person_edit, name='ldb_people_create'),
+
+    url(r'^organizations/(?P<pk>\d+)/$', OrganizationDetailView.as_view(),
+        name='ldb_organizations_detail'),
+    url(r'^organizations/(?P<pk>\d+)/delete/$', OrganizationDeleteView.as_view(),
+        name='ldb_organizations_delete'),
+    url(r'^organizations/(?P<pk>\d+)/edit/$', organization_edit),
+    url(r'^organizations/create/$', organization_edit, name='ldb_organizations_create'),
+
 )
--- ldb/views.py
+++ ldb/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from django.views.generic import DetailView, DeleteView
+from haystack.query import SearchQuerySet
+
+from ldb.forms import OrganizationForm, PersonForm, MemberFormSet, StudentFormSet, AlumnusFormSet, EmployeeFormSet, \
+    CommitteeMembershipFormSet
+from ldb.models import Organization, Person
 
 
 def index(request):
@@ -23,3 +30,140 @@ def index(request):
         post_data['navbar']['items'].append((reverse('admin:ldb_person_changelist'), "Beheer"))
 
     return render_to_response('ldb/dashboard.html', post_data, context_instance=RequestContext(request))
+
+def index_old(request):
+    data = {
+        'title' : 'Ledenadministratie'
+    }
+    return render_to_response( 'ldb/index.html', data,
+                               context_instance = RequestContext(request))
+
+def ajax_people_search(request):
+    if request.is_ajax():
+        q = request.GET.get('q')
+        if q is not None:
+            results    = SearchQuerySet().auto_query(q)
+            # suggestion = results.spelling_suggestion()
+            template   = 'ldb/results.html'
+            data       = {'results': results[:10], 'count': len(results), 'remainder': len(results)-10}
+            return render_to_response( template, data,
+                                       context_instance = RequestContext(request))
+    else:
+        return redirect('ldb_index_old')
+
+class OrganizationDetailView(DetailView):
+    context_object_name = 'organization'
+    model = Organization
+
+class OrganizationDeleteView(DeleteView):
+    context_object_name = 'organization'
+    model = Organization
+    success_url = '/'
+
+def organization_edit(request, pk=None):
+    data = {
+        'title' : 'Ledenadministratie'
+    }
+    if pk:
+        organization = Organization.objects.get(pk=pk)
+    else:
+        organization = Organization()
+    if request.method == 'POST':
+        form = OrganizationForm(request.POST, instance=organization)
+        if form.is_valid():
+            person = form.save()
+            # FIXME: redirect
+            return HttpResponseRedirect(reverse('ldb_organizations_detail', args=(organization.id,)))
+    else:
+        form = OrganizationForm(instance=organization)
+    data['form'] = form
+    return render_to_response('ldb/organization_form.html', data,
+                              context_instance = RequestContext(request))
+
+class PersonDetailView(DetailView):
+    context_object_name = 'person'
+    model = Person
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PersonDetailView, self).get_context_data(**kwargs)
+        # Fetch committee memberships
+        try:
+            context['committee_memberships'] = self.get_object().committeemembership_set.order_by('-board').all()
+        except:
+            pass
+        return context
+
+class PersonDeleteView(DeleteView):
+    context_object_name = 'person'
+    model = Person
+    success_url = '/'
+
+def person_edit(request, pk=None):
+    data = {
+        'title' : 'Ledenadministratie'
+    }
+    if pk:
+        person = Person.objects.get(pk=pk)
+    else:
+        person = Person()
+    if request.method == 'POST':
+        form = PersonForm(request.POST, instance=person)
+        member_formset = MemberFormSet(request.POST, instance=person)
+        student_formset = StudentFormSet(request.POST, instance=person)
+        alumnus_formset = AlumnusFormSet(request.POST, instance=person)
+        employee_formset = EmployeeFormSet(request.POST, instance=person)
+        committeemembership_formset = CommitteeMembershipFormSet(request.POST, instance=person)
+        if form.is_valid() and \
+           member_formset.is_valid() and \
+           student_formset.is_valid() and \
+           alumnus_formset.is_valid and \
+           employee_formset.is_valid() and \
+           committeemembership_formset.is_valid():
+            person = form.save()
+
+            member_form = member_formset.forms[0]
+            if member_form.has_changed():
+                member = member_form.save(commit=False)
+                member.person = person
+                member.save()
+
+            student_form = student_formset.forms[0]
+            if student_form.has_changed():
+                student = student_form.save(commit=False)
+                student.person = person
+                student.save()
+
+            alumnus_form = alumnus_formset.forms[0]
+            if alumnus_form.has_changed():
+                alumnus = alumnus_form.save(commit=False)
+                alumnus.person = person
+                alumnus.save()
+
+            employee_form = employee_formset.forms[0]
+            if employee_form.has_changed():
+                employee = employee_form.save(commit=False)
+                employee.person = person
+                employee.save()
+
+            for form in committeemembership_formset.forms:
+                if form.has_changed():
+                    committeemembership = form.save(commit=False)
+                    committeemembership.person = person
+                    committeemembership.save()
+            return HttpResponseRedirect(reverse('ldb_people_detail', args=(person.id,)))
+    else:
+        form = PersonForm(instance=person)
+        member_formset = MemberFormSet(instance=person)
+        student_formset = StudentFormSet(instance=person)
+        alumnus_formset = AlumnusFormSet(instance=person)
+        employee_formset = EmployeeFormSet(instance=person)
+        committeemembership_formset = CommitteeMembershipFormSet(instance=person)
+    data['form'] = form
+    data['member_formset'] = member_formset
+    data['student_formset'] = student_formset
+    data['alumnus_formset'] = alumnus_formset
+    data['employee_formset'] = employee_formset
+    data['committeemembership_formset'] = committeemembership_formset
+    return render_to_response('ldb/person_form.html', data,
+                              context_instance = RequestContext(request))
