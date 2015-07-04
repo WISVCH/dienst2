9eb90a51b9355f26cabbe987ba324611043f828a
Mark Janssen <mark@praseodym.net>
2015-07-04 16:48:27 +0200

    iexact filtering for NetID and LDAP username fields Fixes #66

--- ldb/views_api.py
+++ ldb/views_api.py
@@ -1,13 +1,23 @@
+import django_filters
 from rest_framework import viewsets
 
 from ldb.models import Person, Organization
 from ldb.serializers import PersonSerializer, OrganizationSerializer
 
 
+class PersonFilter(django_filters.FilterSet):
+    netid = django_filters.CharFilter(name='netid', lookup_type='iexact')
+    ldap_username = django_filters.CharFilter(name='ldap_username', lookup_type='iexact')
+
+    class Meta:
+        model = Person
+        fields = ['ldap_username', 'netid', 'student__student_number']
+
+
 class PersonViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = PersonSerializer
     queryset = Person.objects.all()
-    filter_fields = ('ldap_username', 'netid', 'student__student_number')
+    filter_class = PersonFilter
 
 
 class OrganizationsViewSet(viewsets.ReadOnlyModelViewSet):
