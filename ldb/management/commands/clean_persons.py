93317dd018e7a525f90359cc1b73c7738dd7bcc3
Mark Janssen <mark@praseodym.net>
2015-07-07 16:59:06 +0200

    Update clean persons script

--- ldb/management/commands/clean_persons.py
+++ ldb/management/commands/clean_persons.py
@@ -1,8 +1,11 @@
+from sys import stderr
+
 from django.core.exceptions import ValidationError
 from django.core.management import BaseCommand
 
-from ldb.models import Person
+from ldb.models import Person, Member, Student, Alumnus
 
+BASE_URL = 'https://frans.chnet/dienst2'
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -21,6 +24,11 @@ class Command(BaseCommand):
                             dest='clean-students',
                             default=True,
                             help='Do not run full_clean for students')
+        parser.add_argument('--no-alumni',
+                            action='store_false',
+                            dest='clean-alumni',
+                            default=True,
+                            help='Do not run full_clean for alumni')
         parser.add_argument('--save',
                             action='store_true',
                             dest='save',
@@ -28,34 +36,46 @@ class Command(BaseCommand):
                             help='Perform save on models')
 
     def handle(self, *args, **options):
-        persons = Person.objects.all()
+        persons = Person.objects.all().order_by('id')
         for person in persons:
             if options['clean-persons']:
                 try:
                     person.full_clean()
-                except ValidationError:
-                    self.stderr(
-                        'Validation error in Person https://frans.chnet/dienst2/admin/ldb/person/{}/'.format(person.id))
-
-                if options['save']:
+                    if options['save']:
                         person.save()
+                except ValidationError, e:
+                    stderr.write('Validation error in Person {}/admin/ldb/person/{}/ - {}\n'
+                                 .format(BASE_URL, person.id, e))
 
             if options['clean-members']:
                 try:
                     person.member.full_clean()
-                except ValidationError:
-                    self.stderr(
-                        'Validation error in Member https://frans.chnet/dienst2/admin/ldb/person/{}/'.format(person.id))
-
-                if options['save']:
+                    if options['save']:
                         person.member.save()
+                except Member.DoesNotExist:
+                    pass
+                except ValidationError, e:
+                    stderr.write('Validation error in Member {}/admin/ldb/person/{} - {}/\n'
+                                 .format(BASE_URL, person.id, e))
 
             if options['clean-students']:
                 try:
                     person.student.full_clean()
-                except ValidationError:
-                    self.stderr(
-                        'Validation error in Student https://frans.chnet/dienst2/admin/ldb/person/{}/'.format(person.id))
-
-                if options['save']:
+                    if options['save']:
                         person.student.save()
+                except Student.DoesNotExist:
+                    pass
+                except ValidationError, e:
+                    stderr.write('Validation error in Student {}/admin/ldb/person/{}/ - {}\n'
+                                 .format(BASE_URL, person.id, e))
+
+            if options['clean-alumni']:
+                try:
+                    person.alumnus.full_clean()
+                    if options['save']:
+                        person.alumnus.save()
+                except Alumnus.DoesNotExist:
+                    pass
+                except ValidationError, e:
+                    stderr.write('Validation error in Alumnus {}/admin/ldb/person/{}/ - {}\n'
+                                 .format(BASE_URL, person.id, e))
