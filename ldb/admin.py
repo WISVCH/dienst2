8c968c41496605cc101650e05b1609c249fff632
Sven van Hal <sven@svenvanhal.nl>
2015-09-02 23:26:48 +0200

    Add educational mailing to mailing preferences.

--- ldb/admin.py
+++ ldb/admin.py
@@ -72,7 +72,7 @@ class PersonAdmin(CompareVersionAdmin):
         (_('Other'), {'fields': ['gender', 'birthdate', 'deceased', 'comment']}),
         (_('Subscriptions'), {'fields': ['machazine', 'board_invites', 'constitution_card',
                                          'christmas_card', 'yearbook',
-                                         'mail_announcements', 'mail_company']}),
+                                         'mail_announcements', 'mail_company', 'mail_educational']}),
     ]
     inlines = [MemberInline, CommitteeMembershipInline, StudentInline, AlumnusInline, EmployeeInline]
 
