678a266e60a6530b0b8921b961a2b52f15de1de5
Sven van Hal <sven@svenvanhal.nl>
2015-09-05 13:29:36 +0200

    Fix organization weights.

--- ldb/search_indexes.py
+++ ldb/search_indexes.py
@@ -18,9 +18,9 @@ class PersonIndex(indexes.SearchIndex, indexes.Indexable):
 class OrganizationIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
-    name = indexes.CharField(use_template=True, boost=1.5)
-    address = indexes.CharField(use_template=True, boost=.5)
-    contact = indexes.CharField(use_template=True, boost=.85)
+    name = indexes.CharField(use_template=True, boost=1.1)
+    address = indexes.CharField(use_template=True, boost=.9)
+    contact = indexes.CharField(use_template=True, boost=.95)
 
     def get_model(self):
         return Organization
