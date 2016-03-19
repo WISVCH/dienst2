c31f1cec92cc304068aff5705a327c42283bf588
Jeffrey de Lange <git@jeffreydelange.nl>
2016-03-19 20:23:34 +0100

    Fix url

--- post/urls.py
+++ post/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from post.views import ItemListView, ItemCreateView, ItemWeekArchiveView, AVListView
 
 urlpatterns = patterns('',
-                       url(r'^/$', ItemListView.as_view(), name='post_index'),
+                       url(r'^$', ItemListView.as_view(), name='post_index'),
                        url(r'^create/$', ItemCreateView.as_view(), name='post_create'),
                        url(r'^av/$', AVListView.as_view(), name='post_av'),
                        url(r'^(?P<year>[0-9]{4})/week/(?P<week>[0-9]+)/$',
