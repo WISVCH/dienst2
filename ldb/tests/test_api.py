f3f6e3ccc753007b65b6467db95d77fdd6108fae
Jeffrey de Lange <git@jeffreydelange.nl>
2017-01-08 17:31:01 +0100

    Add tests for membership status in ldb
    Also introduces PersonQuerySet that uses the membership status

--- ldb/tests/test_api.py
+++ ldb/tests/test_api.py
@@ -1,22 +1,34 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from ldb.models import Person
+from ldb.tests.helpers import LDBHelperMixin
 
 
-class ApiV3TestCase(APITestCase):
+class ApiV3TestCase(LDBHelperMixin, APITestCase):
     def setUp(self):
         self.user = User.objects.create()
-        self.client.force_login(self.user)
 
         self.LDAP_USERNAME = 'ldap'
-        self.person_1 = Person.objects.create(ldap_username=self.LDAP_USERNAME)
-        self.person_2 = Person.objects.create()
-        self.person_3 = Person.objects.create()
+
+        self.create_person(ldap_username=self.LDAP_USERNAME)
+        self.create_person()
+        self.create_person()
+
+    def test_token_authentication(self):
+        key = 'welcome'
+        token = Token.objects.create(key=key, user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        url = reverse('person-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_person_list(self):
+        self.client.force_login(self.user)
         url = reverse('person-list')
         response = self.client.get(url)
 
@@ -24,6 +36,7 @@ class ApiV3TestCase(APITestCase):
         self.assertEqual(len(response.data['results']), 3)
 
     def test_person_filter_exists(self):
+        self.client.force_login(self.user)
         url = reverse('person-list')
         response = self.client.get(url, {'ldap_username': self.LDAP_USERNAME})
 
@@ -31,6 +44,7 @@ class ApiV3TestCase(APITestCase):
         self.assertEqual(len(response.data['results']), 1)
 
     def test_person_filter_not_exists(self):
+        self.client.force_login(self.user)
         url = reverse('person-list')
         response = self.client.get(url, {'ldap_username': 'does not exist'})
 
