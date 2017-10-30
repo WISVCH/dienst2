a575236fcc17e1cb18d187e78e5d9cdd542d83c5
Mark Janssen <mark@praseodym.net>
2017-10-30 21:12:39 +0100

    Wait for Elasticsearch to start before indexing (#180)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 if [ "$1" = 'gunicorn' ]; then
     ./manage.py migrate --noinput
+    until curl -sf http://localhost:9200 > /dev/null; do 
+        echo 'Waiting for Elasticsearch'
+        sleep 1
+    done
     ./manage.py rebuild_index --noinput &
     exec "$@" -b 0.0.0.0 dienst2.wsgi
 fi
