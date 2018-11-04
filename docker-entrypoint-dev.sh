8da3aa8f8513dc6a5e906d375e50f34f1b98dbfa
Mark Janssen <mark@praseodym.net>
2018-11-04 18:13:23 +0100

    Enable Python+Django warnings during development

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -11,7 +11,7 @@ if [ "$1" = 'dev' ]; then
     ./manage.py migrate --noinput
     ./manage.py createdevsuperuser --username admin --password admin --email admin@example.com
     echo Running server on http://localhost:8000, log in with admin / admin
-    exec ./manage.py runserver 0.0.0.0:8000
+    exec python -Wall ./manage.py runserver 0.0.0.0:8000
 fi
 
 exec "$@"
