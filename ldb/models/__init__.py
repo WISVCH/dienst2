3c2a2efb531d20228209295491af9a6b07cec365
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2020-01-10 10:24:24 +0100

    add exports to init.py

---
+++ ldb/models/__init__.py
@@ -0,0 +1,15 @@
+from .alumnus import Alumnus
+from .committee import Committee
+from .committeeMemberShip import CommitteeMembership
+from .employee import Employee
+from .entity import Entity
+from .member import Member
+from .membershipStatus import MembershipStatus
+from .membershipStatusField import MembershipStatusField
+from .modifaction import Modification
+from .organization import Organization
+from .person import Person
+from .student import Student
+
+__all__ = ['Alumnus', 'Committee', 'Person', 'Employee', 'Entity', 'Member', 'MembershipStatusField', 'Modification',
+           'MembershipStatus', 'CommitteeMembership', 'Organization', 'Person', 'Student']
