448b4d1757fafe089c8913350036d0acf42a9acd
julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2020-04-01 02:33:01 +0200

    Add example form with required functionality

--- src/main.go
+++ src/main.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.LoadHTMLGlob("./templates/*")
+
+	r.POST("/form", MemberFormHandler)
+	r.GET("/form", MemberFormRender)
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
