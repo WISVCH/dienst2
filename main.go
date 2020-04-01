5ff62ddb7a7bd00f1be97fbad514ba0a1231bb77
julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2020-04-01 14:13:53 +0200

    remove import cyclus

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"./server"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("./templates/*")
 
-	r.POST("/form", MemberFormHandler)
-	r.GET("/form", MemberFormRender)
+	r.POST("/form", server.MemberFormHandler)
+	r.GET("/form", server.MemberFormRender)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
