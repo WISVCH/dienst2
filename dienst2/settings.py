67100fd1f48e320b1b450fdad0a658842386ab15
Joep de Jong <joep@joepdejong.com>
2023-02-11 12:12:03 +0100

    Change cache to locmemcache

--- dienst2/settings.py
+++ dienst2/settings.py
@@ -18,7 +18,10 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 DATABASES = {"default": env.db()}
 
-CACHES = {"default": env.cache()}
+CACHES = env(
+    "CACHES",
+    default={"default": {"BACKEND": "django.core.cache.backends.dummy.LocMemCache"}},
+)
 
 HAYSTACK_CONNECTIONS = {"default": env.search_url()}
 HAYSTACK_SIGNAL_PROCESSOR = "haystack.signals.RealtimeSignalProcessor"
