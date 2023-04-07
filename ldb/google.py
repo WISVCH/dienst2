9b05eb71cf1d3cf9e0cf6ac5456ed8cbccbdf118
Joep de Jong <joep@joepdejong.com>
2023-04-07 20:29:35 +0000

    Run black

--- ldb/google.py
+++ ldb/google.py
@@ -11,10 +11,13 @@ def get_google_service(scopes=[]):
     """Returns a Google Directory API service object"""
     # Load the service account from the project root
     service_account_filepath = "google-service-account.json"
-    credentials = service_account.Credentials.from_service_account_file(service_account_filepath, scopes=scopes)
-    delegated_credentials = credentials.with_subject(env.str("GOOGLE_SERVICE_ACCOUNT_DELEGATED_USER"))
+    credentials = service_account.Credentials.from_service_account_file(
+        service_account_filepath, scopes=scopes
+    )
+    delegated_credentials = credentials.with_subject(
+        env.str("GOOGLE_SERVICE_ACCOUNT_DELEGATED_USER")
+    )
     return build("admin", "directory_v1", credentials=delegated_credentials)
-
 
 
 def get_groups_by_user_key(userKey, domains=["wisv.ch", "ch.tudelft.nl"]) -> list:
@@ -25,7 +28,7 @@ def get_groups_by_user_key(userKey, domains=["wisv.ch", "ch.tudelft.nl"]) -> lis
     :param domains: Domains to search for groups. Ensure that these are set to prevent group name attacks by using other domains.
 
     :return: List of group email addresses
-    
+
     (https://developers.google.com/admin-sdk/directory/reference/rest/v1/groups/list)
     """
 
@@ -39,7 +42,7 @@ def get_groups_by_user_key(userKey, domains=["wisv.ch", "ch.tudelft.nl"]) -> lis
         ]
     )
 
-    groups : list = []
+    groups: list = []
     for domain in domains:
         data = service.groups().list(userKey=userKey, domain=domain).execute()
         if "groups" in data:
@@ -47,6 +50,7 @@ def get_groups_by_user_key(userKey, domains=["wisv.ch", "ch.tudelft.nl"]) -> lis
                 groups.append(group["email"])
 
     return groups
+
 
 def get_parent_group(group: str):
     """
@@ -75,17 +79,18 @@ def get_book_year() -> int:
 
     if now.month < 9:
         return now.year - 1
-    
+
     return now.year
+
 
 def get_board_year() -> int:
     """
-        Return the current board year.
-        Under the assumption that the board year starts at the 1st of September and that from now on only complete years are made.
+    Return the current board year.
+    Under the assumption that the board year starts at the 1st of September and that from now on only complete years are made.
     """
     return get_book_year() - 1956
 
-    
+
 YEAR = str(get_book_year())
 BOARD_YEAR = str(get_board_year())
 HARDCODED_PARENT_GROUPS = {
@@ -93,11 +98,31 @@ HARDCODED_PARENT_GROUPS = {
     "vc": ["vc_" + YEAR + "@ch.tudelft.nl"],
     "galacie@ch.tudelft.nl": ["galacie" + YEAR + "@ch.tudelft.nl"],
     "hackdelft-commissie@ch.tudelft.nl": ["hackdelft_" + YEAR + "@ch.tudelft.nl"],
-    "icom-commissie@ch.tudelft.nl": ["icom_" + YEAR + "@ch.tudelft.nl"]
+    "icom-commissie@ch.tudelft.nl": ["icom_" + YEAR + "@ch.tudelft.nl"],
 }
 
 SENAAT_GROUP = "senaat@ch.tudelft.nl"
-HARDCODED_COMMITTEE_GROUPS = ["akcie", "annucie", "choco", "coh", "comma", "dies", "eiweiw", "facie", "flitcie", "lancie", "machazine", "maphya", "match", "sjaarcie", "symposium",  "wiewie", "wifi", "wocky"]
+HARDCODED_COMMITTEE_GROUPS = [
+    "akcie",
+    "annucie",
+    "choco",
+    "coh",
+    "comma",
+    "dies",
+    "eiweiw",
+    "facie",
+    "flitcie",
+    "lancie",
+    "machazine",
+    "maphya",
+    "match",
+    "sjaarcie",
+    "symposium",
+    "wiewie",
+    "wifi",
+    "wocky",
+]
+
 
 def get_parent_committee_group(group: str) -> str:
     """
@@ -117,8 +142,9 @@ def get_parent_committee_group(group: str) -> str:
 
     if year == YEAR and committee in HARDCODED_COMMITTEE_GROUPS:
         return committee + "@" + group.split("@")[1]
-    
+
     return None
+
 
 def get_indirect_groups(groups: list) -> list:
     """
@@ -138,7 +164,7 @@ def get_indirect_groups(groups: list) -> list:
         # parent_group = get_parent_group(group)
         # if parent_group is not None:
         #     groups.append(parent_group)
-        
+
         # Check if group begins with "bestuur" or "board"
         if group.startswith("bestuur") or group.startswith("board"):
             if group.endswith("@ch.tudelft.nl"):
@@ -154,6 +180,7 @@ def get_indirect_groups(groups: list) -> list:
                 indirect_groups.append(key)
 
     return indirect_groups
+
 
 if __name__ == "__main__":
     groups = get_groups_by_user_key("joepj@ch.tudelft.nl")
